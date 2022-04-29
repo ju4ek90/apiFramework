207504567cc77fea1bf4d8a40893f8b9e3c513f2
yanisimova <yanisimova@magnite.com>
2022-04-29 16:12:06 +0300

    apiFramework

--- app/api/api_client.py
+++ app/api/api_client.py
@@ -15,38 +15,45 @@ class ApiClient(BaseRequests):
         self.token = r.json().get('token')
         return self.token
 
-    def get_root_folder(self, token):
+    def get_root_folder(self):
         """Method to get root folder"""
-        r = self.get(Urls.ROOT_FOLDER,
-                     headers={'x-token': token})
-        return r.status_code
-
-    def get_specific_folder(self, token):
-        """Method to get specific folder"""
-        r = self.get(Urls.SPECIFIC_FOLDER,
-                     headers={'x-token': token})
-        return r.status_code
-
-    def get_count(self, token):
-        """Method to get count folder"""
-        r = self.get(Urls.FILES_COUNT_URL,
-                     headers={'x-token': token})
+        r = self.get(Urls.FILES_V2,
+                     params={'breadcrumbs': '1', 'offset': '0', 'limit': '1000', '_': '1622700773180'},
+                     headers={'x-token': self.token})
         return r.json()
 
-    def get_runs(self, token):
+    def get_specific_folder(self):
+        """Method to get specific folder"""
+        r = self.get(Urls.FILES_V2,
+                     params={'breadcrumbs': '1', 'offset': '0', 'limit': '1000',
+                             'folder_id': '84c966d5-8dce-429d-8f92-44d5e28b1581', '_': '1622700773180'},
+                     headers={'x-token': self.token})
+        return r.json()
+
+    def get_count(self):
+        """Method to get count folder"""
+        r = self.get(Urls.FILES_COUNT_URL,
+                     params={'folder_id': '84c966d5-8dce-429d-8f92-44d5e28b1581', '_': '1622700773179'},
+                     headers={'x-token': self.token})
+        return r.json()
+
+    def get_runs(self):
         """Method to get runs requests"""
-        r = self.get(Urls.FILES_RESULT_URL_GET_RUN,
-                     headers={'x-token': token})
-        return r.status_code
+        r = self.get(Urls.FILES_RUN.format('7f4c7326-0a4e-4b56-a8d0-8ce002191672'),
+                     params={'_': '1622700773181'},
+                     headers={'x-token': self.token})
+        return r.json()
 
-    def get_analyses(self, token):
+    def get_analyses(self):
         """Method to get analyses requests"""
-        r = self.get(Urls.FILES_RESULT_URL_GET_ANALYSES,
-                     headers={'x-token': token})
-        return r.status_code
+        r = self.get(Urls.FILES_ANALYSES.format('437ef8e4-b595-4fd8-a2f5-64221831e925'),
+                     params={'_': '1622700773184'},
+                     headers={'x-token': self.token})
+        return r.json()
 
-    def get_artifacts(self, token):
+    def get_artifacts(self):
         """Method to get artifacts requests"""
-        r = self.get(Urls.FILES_RESULT_URL_GET_ARTIFACTS,
-                     headers={'x-token': token})
-        return r.status_code
+        r = self.get(Urls.FILES_ANALYSES.format('437ef8e4-b595-4fd8-a2f5-64221831e925'),
+                     params={'_': '1622700773185'},
+                     headers={'x-token': self.token})
+        return r.json()
--- tests/tests_requests_folders.py
+++ tests/tests_requests_folders.py
@@ -1,34 +1,34 @@
 import pytest as pytest
 
-
 from app.api.api_client import ApiClient
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def get_token():
-    return ApiClient().login()
+    ac = ApiClient()
+    ac.login()
+    yield ac
 
 
 def test_root_folder_succesfull(get_token):
-    assert ApiClient().get_root_folder(get_token) is 200
+    assert get_token.get_root_folder() is not None
 
 
 def test_specific_folder_succesfull(get_token):
-    assert ApiClient().get_specific_folder(get_token) is 200
+    assert get_token.get_specific_folder() is not None
 
 
 def test_count_succesfull(get_token):
-    assert ApiClient().get_count(get_token) is not None
+    assert get_token.get_count() is not None
 
 
 def test_runs_succesfull(get_token):
-    assert ApiClient().get_runs(get_token) is 200
+    assert get_token.get_runs() is not None
 
 
 def test_analyses_succesfull(get_token):
-    assert ApiClient().get_analyses(get_token) is 200
+    assert get_token.get_analyses() is not None
 
 
 def test_artifacts_succesfull(get_token):
-    assert ApiClient().get_artifacts(get_token) is 200
-
+    assert get_token.get_artifacts() is not None
