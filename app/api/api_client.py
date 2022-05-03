7e72786da6ea582e65e9497348db09856c4da2f7
yanisimova <yanisimova@magnite.com>
2022-05-03 23:31:10 +0300

    apiFramework

--- app/api/api_client.py
+++ app/api/api_client.py
@@ -5,55 +5,46 @@ from config.config import Config
 
 class ApiClient(BaseRequests):
 
-    def __init__(self) -> None:
-        self.token = None
-
     def login(self):
         """Method to execute login"""
-        r = self.get(Urls.LOGIN,
+        r = self.post(path=Urls.LOGIN,
                      headers={'Authorization': f'Basic {Config.LOGIN_TOKEN}'})
-        self.token = r.json().get('token')
-        return self.token
+        token = r.json().get('token')
+        self.set_token(token)
 
     def get_root_folder(self):
         """Method to get root folder"""
-        r = self.get(Urls.FILES_V2,
-                     params={'breadcrumbs': '1', 'offset': '0', 'limit': '1000', '_': '1622700773180'},
-                     headers={'x-token': self.token})
+        r = self.get(path=Urls.FILES_V2,
+                     params={'breadcrumbs': '1', 'offset': '0', 'limit': '1000', '_': '1622700773180'})
         return r.json()
 
     def get_specific_folder(self):
         """Method to get specific folder"""
-        r = self.get(Urls.FILES_V2,
+        r = self.get(path=Urls.FILES_V2,
                      params={'breadcrumbs': '1', 'offset': '0', 'limit': '1000',
-                             'folder_id': '84c966d5-8dce-429d-8f92-44d5e28b1581', '_': '1622700773180'},
-                     headers={'x-token': self.token})
+                             'folder_id': '84c966d5-8dce-429d-8f92-44d5e28b1581', '_': '1622700773180'})
         return r.json()
 
     def get_count(self):
         """Method to get count folder"""
-        r = self.get(Urls.FILES_COUNT_URL,
-                     params={'folder_id': '84c966d5-8dce-429d-8f92-44d5e28b1581', '_': '1622700773179'},
-                     headers={'x-token': self.token})
+        r = self.get(path=Urls.FILES_COUNT_URL,
+                     params={'folder_id': '84c966d5-8dce-429d-8f92-44d5e28b1581', '_': '1622700773179'})
         return r.json()
 
     def get_runs(self):
         """Method to get runs requests"""
         r = self.get(Urls.FILES_RUN.format('7f4c7326-0a4e-4b56-a8d0-8ce002191672'),
-                     params={'_': '1622700773181'},
-                     headers={'x-token': self.token})
+                     params={'_': '1622700773181'})
         return r.json()
 
     def get_analyses(self):
         """Method to get analyses requests"""
         r = self.get(Urls.FILES_ANALYSES.format('437ef8e4-b595-4fd8-a2f5-64221831e925'),
-                     params={'_': '1622700773184'},
-                     headers={'x-token': self.token})
+                     params={'_': '1622700773184'})
         return r.json()
 
     def get_artifacts(self):
         """Method to get artifacts requests"""
         r = self.get(Urls.FILES_ANALYSES.format('437ef8e4-b595-4fd8-a2f5-64221831e925'),
-                     params={'_': '1622700773185'},
-                     headers={'x-token': self.token})
+                     params={'_': '1622700773185'})
         return r.json()
--- app/api/base_request.py
+++ app/api/base_request.py
@@ -1,3 +1,5 @@
+import urllib
+
 from config.config import Config
 import requests
 
@@ -5,16 +7,25 @@ import requests
 class BaseRequests:
     """Class for calling HTTP requests"""
 
+    def __init__(self) -> None:
+        self.token = None
+        self.headers = None
+
+    def set_token(self, token):
+        self.token = token
+        self.headers = {'x-token': token}
+
     def form_url(self, url):
         """Method to concat base url and api path"""
-        return Config.BASE_URL + url
+        return urllib.parse.urljoin(Config.BASE_URL, url)
 
-    def get(self, path, *args, **kwargs):
+    def get(self, path, headers={}, *args, **kwargs):
         """Reimplementation of GET method"""
         url = self.form_url(path)
-        return requests.get(url, *args, **kwargs)
+        headers = {**self.headers, **headers}
+        return requests.get(url=url, headers=headers, *args, **kwargs)
 
-    def post(self, path, params):
+    def post(self, path, *args, **kwargs):
         """Reimplementation of POST method"""
         url = self.form_url(path)
-        return requests.post(url, params)
+        return requests.post(url, *args, **kwargs)
